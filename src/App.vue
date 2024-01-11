689bcca98fcb069e87c626cbe3f55375cefc2818
LucaFormica17 <145986784+LucaFormica17@users.noreply.github.com>
2024-01-11 12:50:51 +0100

    [UPDATE] new project

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+
+  export default {
+  components:{
+    AppHeader,
+    AppMain
+  }    
+  }
+</script>
+<template lang="">
+  <div>
+    <AppHeader />
+    <AppMain />
+  </div>
+</template>
+<style lang="">
+  
+</style>
